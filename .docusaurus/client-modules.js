c0e359fca6ead04e249b933cbca587fee9bbcfcd
flytianya2010 <huafly883@163.com>
2022-01-02 10:22:27 +0800

    update

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("F:\\docusaurus\\DocusaurusBearPi\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("F:\\docusaurus\\DocusaurusBearPi\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("F:\\docusaurus\\DocusaurusBearPi\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css"),
+  require("F:\\docusaurus\\DocusaurusBearPi\\src\\css\\custom.css"),
+];
