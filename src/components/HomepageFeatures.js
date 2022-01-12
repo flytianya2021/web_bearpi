e13c41d0ab9c56119e04ad429b209b92bd410422
flytianya2010 <huafly883@163.com>
2022-01-12 10:06:33 +0800

    git push origin master

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: '小熊派 IoT',   
-    Svg: require('../../static/img/bearpi_st.svg').default,
+    Svg: require('../../static/img/iot_st.svg').default,
     description: (
       <>
        物联网开发板，华为官方指定开发板。可实现智慧农业、智慧物流、智慧路灯等案例。
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: '小熊派-鸿蒙-叔',
-    Svg: require('../../static/img/bearpi_hm_micro.svg').default,
+    Svg: require('../../static/img/hm_micro.svg').default,
     description: (
       <>
        鸿蒙开发板，全球首款可折叠开发板。
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: '小熊派-鸿蒙-季',
-    Svg: require('../../static/img/bearpi_hm_nano.svg').default,
+    Svg: require('../../static/img/hm_nano.svg').default,
     description: (
       <>
         鸿蒙入门开发板
